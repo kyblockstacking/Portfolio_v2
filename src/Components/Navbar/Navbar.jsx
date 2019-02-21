e5ee1dad9e246f90d7c5aefa9be9c5d85f472ef3
knyg <kevin1992yang@gmail.com>
2019-02-20 18:55:45 -0800

    reposition navbar and center icons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,15 +5,17 @@ class Navbar extends React.Component {
   render() {
 
     return (
-      <div className='navbar fade-in-top'>
-        <span className='logo bounce-in-top'>KY</span>
-        <a href='#contact' className='navLinks'>CONTACT</a>
-        <span className='separateLinks'>&nbsp;|&nbsp;</span>
-        <a href='#about' className='navLinks'>ABOUT</a>
-        <span className='separateLinks'>&nbsp;|&nbsp;</span>
-        <a href='#stack' className='navLinks'>STACK</a>
-        <span className='separateLinks'>&nbsp;|&nbsp;</span>
-        <a href='https://github.com/kyblockstacking/Resume' target='_blank' rel='noopener noreferrer' className='navLinks'>RÉSUMÉ</a>
+      <div>
+        <div className='logo bounce-in-top'>Kevin Yang</div>
+        <div className='navbar fade-in-top'>
+          <a href='#contact' className='navLinks'>CONTACT</a>
+          <span className='separateLinks'>&nbsp;|&nbsp;</span>
+          <a href='#about' className='navLinks'>ABOUT</a>
+          <span className='separateLinks'>&nbsp;|&nbsp;</span>
+          <a href='#stack' className='navLinks'>STACK</a>
+          <span className='separateLinks'>&nbsp;|&nbsp;</span>
+          <a href='https://github.com/kyblockstacking/Resume' target='_blank' rel='noopener noreferrer' className='navLinks'>RÉSUMÉ</a>
+        </div>
       </div>
     );
   };
--- src/Components/Stack/Stack.jsx
+++ src/Components/Stack/Stack.jsx
@@ -8,31 +8,37 @@ class Stack extends Component {
         return (
             <div className='stackContainer'>
 
-                <i className='fab fa-react icons'><span className='iconNames'>React</span></i>
-                <i className='fab fa-js-square icons'><span className='iconNames'>JavaScript</span></i>
-                <i className='fas fa-rss icons'><span className='iconNames'>JQuery</span></i>
-                <i className='fas fa-retweet icons'><span className='iconNames'>AJAX</span></i>
-                <i className='fab fa-html5 icons'><span className='iconNames'>HTML</span></i>
-                <i className='fab fa-css3 icons'><span className='iconNames'>CSS</span></i>
-                <i className='fas fa-box-open icons'><span className='iconNames'>FlexBox</span></i>
-                <i className='fab fa-sass icons'><span className='iconNames'>SASS</span></i>
-                <i className='fas fa-bold icons'><span className='iconNames'>Bootstrap</span></i>
-                <i className='fab fa-node icons'><span className='iconNames'>Node</span></i>
-                <i className='fas fa-server icons'><span className='iconNames'>Express</span></i>
-                <i className='fab fa-yarn icons'><span className='iconNames'>Yarn</span></i>
-                <i className='fab fa-git icons'><span className='iconNames'>Git</span></i>
-                <i className='fab fa-github icons'><span className='iconNames'>GitHub</span></i>
-                <i className='fas fa-fish icons'><span className='iconNames'>MySQL</span></i>
-                <i className='fab fa-envira icons'><span className='iconNames'>MongoDB</span></i>
-                <i className='fas fa-code-branch icons'><span className='iconNames'>RegEx</span></i>
-
-                <i className='fas fa-cogs icons'><span className='iconNames'>APIs</span></i>
-                <i className='fab fa-connectdevelop icons'><span className='iconNames'>ES Lint</span></i>
-                <i className='fab fa-korvue icons'><span className='iconNames'>Heroku</span></i>
-                <i className='fas fa-terminal icons'><span className='iconNames'>Command Line</span></i>
-                <i className='icons babelIcon'>Babel<span className='iconNames babelName'>Babel</span></i>
-                <i className='fab fa-npm icons'><span className='iconNames'>Node Package Manager</span></i>
-                <i className='fab fa-aws icons'><span className='iconNames'>Amazon Web Services</span></i>
+                <div className='row1'>
+                    <i className='fab fa-react icons'><span className='iconNames'>React</span></i>
+                    <i className='fab fa-js-square icons'><span className='iconNames'>JavaScript</span></i>
+                    <i className='fas fa-rss icons'><span className='iconNames'>JQuery</span></i>
+                    <i className='fas fa-retweet icons'><span className='iconNames'>AJAX</span></i>
+                    <i className='fab fa-connectdevelop icons'><span className='iconNames'>ES Lint</span></i>
+                    <i className='fab fa-html5 icons'><span className='iconNames'>HTML</span></i>
+                    <i className='fab fa-css3 icons'><span className='iconNames'>CSS</span></i>
+                </div>
+                <div className='row2'>
+                    <i className='fas fa-box-open icons'><span className='iconNames'>FlexBox</span></i>
+                    <i className='fab fa-sass icons'><span className='iconNames'>SASS</span></i>
+                    <i className='fas fa-bold icons'><span className='iconNames'>Bootstrap</span></i>
+                    <i className='fab fa-node icons'><span className='iconNames'>Node</span></i>
+                    <i className='fas fa-server icons'><span className='iconNames'>Express</span></i>
+                    <i className='fab fa-yarn icons'><span className='iconNames'>Yarn</span></i>
+                </div>
+                <div className='row3'>
+                    <i className='fab fa-git icons'><span className='iconNames'>Git</span></i>
+                    <i className='fab fa-github icons'><span className='iconNames'>GitHub</span></i>
+                    <i className='fas fa-fish icons'><span className='iconNames'>MySQL</span></i>
+                    <i className='fab fa-envira icons'><span className='iconNames'>MongoDB</span></i>
+                    <i className='fab fa-korvue icons'><span className='iconNames'>Heroku</span></i>
+                    <i className='fas fa-cogs icons'><span className='iconNames'>APIs</span></i>
+                    <i className='fas fa-code-branch icons'><span className='iconNames'>RegEx</span></i>
+                </div>
+                <div className='row4'>
+                    <i className='fab fa-npm icons'><span className='iconNames'>Node Package Manager</span></i>
+                    <i className='icons babelIcon'>Babel<span className='iconNames babelName'>Babel</span></i>
+                    <i className='fab fa-aws icons'><span className='iconNames'>Amazon Web Services</span></i>
+                </div>
 
             </div>
         );
