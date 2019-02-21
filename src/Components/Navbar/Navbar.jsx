3a68a16eea9bee13da28262375181b7ff004c1d3
knyg <kevin1992yang@gmail.com>
2019-02-20 18:12:22 -0800

    change navbar hover animation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,11 +8,11 @@ class Navbar extends React.Component {
       <div className='navbar fade-in-top'>
         <span className='logo bounce-in-top'>KY</span>
         <a href='#contact' className='navLinks'>CONTACT</a>
-        <span className='seperateLinks'>&nbsp;|&nbsp;</span>
+        <span className='separateLinks'>&nbsp;|&nbsp;</span>
         <a href='#about' className='navLinks'>ABOUT</a>
-        <span className='seperateLinks'>&nbsp;|&nbsp;</span>
+        <span className='separateLinks'>&nbsp;|&nbsp;</span>
         <a href='#stack' className='navLinks'>STACK</a>
-        <span className='seperateLinks'>&nbsp;|&nbsp;</span>
+        <span className='separateLinks'>&nbsp;|&nbsp;</span>
         <a href='https://github.com/kyblockstacking/Resume' target='_blank' rel='noopener noreferrer' className='navLinks'>RÉSUMÉ</a>
       </div>
     );
