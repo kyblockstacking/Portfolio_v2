0ca7d9465eaa7eb63c4e9dbfaee8e7c66efa7c5a
knyg <kevin1992yang@gmail.com>
2019-02-22 01:20:56 -0800

    add some dummy footer content

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,9 +4,28 @@ class Contact extends Component {
     render() {
         return (
             <div className='contact'>
-                <div className='word'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas officia totam magnam quae eos et accusamus quod. Explicabo veniam, sed adipisci aspernatur quisquam odio, natus dicta animi ab veritatis omnis!
+
+                <div className='footer'>
+                    <div className='column'>
+                        <div className='row'>TITLE HERE</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                    </div>
+                    <div className='column special'>
+                        <div className='row'>TITLE HERE</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                    </div>
+                    <div className='column'>
+                        <div className='row'>TITLE HERE</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                        <div className='row'>subtitle here</div>
+                    </div>
                 </div>
+
             </div>
         );
     };
