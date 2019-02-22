4bd5051dc2689c69ade09d2afd96f6115fee9ad9
knyg <kevin1992yang@gmail.com>
2019-02-22 00:23:19 -0800

    initial footer/contact

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Name from './Components/Name/Name.jsx';
 import Hero from './Components/Jumbotron/Jumbotron.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Stack from './Components/Stack/Stack.jsx';
+import Contact from './Components/Contact/Contact.jsx';
 import BackToTop from './Components/BackToTop/BackToTop.jsx';
 
 //sass css
@@ -13,6 +14,7 @@ import './Components/Name/Name.scss';
 import './Components/Jumbotron/Jumbotron.scss';
 import './Components/Navbar/Navbar.scss';
 import './Components/Stack/Stack.scss';
+import './Components/Contact/Contact.scss';
 import './Components/BackToTop/BackToTop.scss';
 
 class App extends Component {
@@ -24,6 +26,7 @@ class App extends Component {
         <Navbar />
         <Hero />
         <Stack />
+        <Contact />
         <BackToTop />
       </div>
     );
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className='contact'>
+                <div className='word'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas officia totam magnam quae eos et accusamus quod. Explicabo veniam, sed adipisci aspernatur quisquam odio, natus dicta animi ab veritatis omnis!
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Contact;
