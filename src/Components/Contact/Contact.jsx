b4dcf6ec25bebd4220d61dc88c60642f9ada9dad
knyg <kevin1992yang@gmail.com>
2019-02-23 19:29:35 -0800

    adjust footer stuff

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,9 +11,9 @@ class Contact extends Component {
                     <div className='row'>
                         <img src={profilePicture} alt='Smiley face' className='profilePicture' />
                         <div className='description'>
-                            <div><i className='fas fa-user-tie'>&nbsp;</i>Kevin Yang</div>
-                            <div><i className='far fa-envelope'>&nbsp;</i>kevin1992yang@gmail.com</div>
-                            <div><i className='fas fa-mobile-alt'>&nbsp;</i>123-123-1234</div>
+                            <div className='footerName'>Kevin Yang</div>
+                            <div><a href='mailto:kevin1992yang@gmail.com' className='contacts'>kevin1992yang@gmail.com</a></div>
+                            <div><a href='tel:626-539-9092' className='contacts'>626-539-9092</a></div>
                         </div>
                     </div>
                     <div className='row'>
