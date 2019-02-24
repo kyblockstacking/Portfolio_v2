dc760163dcd853233c2e40b177958521be95d156
knyg <kevin1992yang@gmail.com>
2019-02-23 19:02:19 -0800

    some footer information

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
 
+import profilePicture from '../../Images/pic.jpg'
+
 class Contact extends Component {
     render() {
         return (
             <div className='contact'>
 
-                <div className='footer'>
-                    <div className='column'>
-                        <div className='row'>TITLE HERE</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
+                <div className='col'>
+                    <div className='row'>
+                        <img src={profilePicture} alt='Smiley face' className='profilePicture' />
+                        <div className='description'>
+                            <div><i className='fas fa-user-tie'>&nbsp;</i>Kevin Yang</div>
+                            <div><i className='far fa-envelope'>&nbsp;</i>kevin1992yang@gmail.com</div>
+                            <div><i className='fas fa-mobile-alt'>&nbsp;</i>123-123-1234</div>
+                        </div>
                     </div>
-                    <div className='column special'>
-                        <div className='row'>TITLE HERE</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
-                    </div>
-                    <div className='column'>
-                        <div className='row'>TITLE HERE</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
-                        <div className='row'>subtitle here</div>
+                    <div className='row'>
+                        <div className='links'><i className='fab fa-github-alt'></i></div>
+                        <div className='links'><i className='fab fa-linkedin-in'></i></div>
+                        <div className='links'><i className='fab fa-reddit-alien'></i></div>
                     </div>
                 </div>
-
             </div>
+
+
         );
     };
 };
